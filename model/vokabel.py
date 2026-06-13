97d08323dae5675946c2a0763a547fab6be9f1e8
Anton Sokolov <sokolov.anton.ev@gmail.com>
2026-06-13 13:07:57 +0300

    Vokabel logic added

---
+++ model/vokabel.py
@@ -0,0 +1,17 @@
+class Vokabel:
+    def __init__(self, deutsch, english):
+        self.deutsch = deutsch
+        self.english = english
+
+    def pruefe(self, antwort):
+        antwort = antwort.strip.lower() #löscht alle Abstände und macht alle Buchstaben klein
+
+        return antwort == self.english.lower()
+
+    # Defines how the vocabulary is displayed as text
+    def __str__(self):
+        return f"{self.deutsch} -> {self.english}"
+
+
+wort = Vokabel("Apfel", "Apple")
+print(wort)
